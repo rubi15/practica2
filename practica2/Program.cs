1f1e35f1d049b361b01f45df645aae86fd46395f
rubi15 <rubi_chica_banda@hotmail.com>
2013-09-19 20:52:04 -0500

    practica#2

---
+++ practica2/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: AspireOne
+ * Date: 19/09/2013
+ * Time: 10:16 a.m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace practica2
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
